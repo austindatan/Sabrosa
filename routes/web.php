7a5cce947534824ef0e53d211782c705fdd437e6
BoneOg <102812700+BoneOg@users.noreply.github.com>
2025-04-30 08:32:24 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'showHome'])->name('home');
-Route::get('/tropical', [ProductController::class, 'showTropical'])->name('tropical.show');
-Route::get('/don', [ProductController::class, 'showDon'])->name('don.show');
-Route::get('/barbie', [ProductController::class, 'showBarbie'])->name('barbie.show');
-Route::get('/bun', [ProductController::class, 'showBun'])->name('bun.show');
-Route::get('/tea', [ProductController::class, 'showTea'])->name('tea.show');
-Route::get('/smores', [ProductController::class, 'showSmores'])->name('smores.show');
+Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
