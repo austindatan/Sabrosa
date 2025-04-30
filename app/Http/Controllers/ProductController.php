1e32501bf3bc113d1f568b088e7555330f7626bc
BoneOg <102812700+BoneOg@users.noreply.github.com>
2025-04-30 08:32:09 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,44 +7,14 @@ use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    public function showTropical(): View
-    {
-        $product = Product::findOrFail(1);
-        return view('tropical', compact('product'));
-    }
-
-    public function showDon(): View
-    {
-        $product = Product::findOrFail(2);
-        return view('don', compact('product'));
-    }
-
-    public function showBarbie(): View
-    {
-        $product = Product::findOrFail(3); 
-        return view('barbie', compact('product'));
-    }
-
-    public function showBun(): View
-    {
-        $product = Product::findOrFail(4);  
-        return view('bun', compact('product'));
-    }
-
-    public function showTea(): View
-    {
-         $product = Product::findOrFail(5); 
-        return view('tea', compact('product'));
-    }
-
-    public function showSmores(): View
-    {
-        $product = Product::findOrFail(6); 
-        return view('smores', compact('product'));
-    }
 
     public function showHome(): View
     {
         return view('home');
+    }
+    
+    public function show(Product $product): View
+    {
+        return view('product_views.product', compact('product'));
     }
 }
