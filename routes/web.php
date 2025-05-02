006d5cd9b3dd61ffde4130f87681f2733faae858
BoneOg <102812700+BoneOg@users.noreply.github.com>
2025-05-02 23:32:50 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'showHome'])->name('home');
 Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
+Route::view('/register', 'authentication.register')->name('register');
+Route::view('/login', 'authentication.login')->name('login');
